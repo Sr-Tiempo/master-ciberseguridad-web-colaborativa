59a1e39821fdb777548fb10438b36fab1f81c367
Sr-Tiempo <srtempotiempo@gmail.com>
2025-05-11 16:23:38 +0200

    fix path traversal - Manuel Acosta Salguero

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -13,10 +13,18 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+		if(esValido(username)){
+			User u = new User(username, HashUtils.getMd5(password), type, -1);
+			u.save();
+			registerComplete();
+		}else{
+			renderTemplate("errors/error_username.html");
+		}
     }
+	
+	public static boolean esValido(String username){
+		return username != null && username.matches("^[A-Za-z]{3,8}$");
+	}
 
     public static void registerComplete(){
         render();
